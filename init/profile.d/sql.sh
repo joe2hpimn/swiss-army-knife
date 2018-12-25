7e5d576c33edaa3cd0701b7ab462eee66211db45
Tingfang Bao <bbao@pivotal.io>
2018-12-25 14:59:37 +0800

    add sql-remote-postgres-fdw-env for mdw-b server

--- init/profile.d/sql.sh
+++ init/profile.d/sql.sh
@@ -21,3 +21,34 @@ CREATE FOREIGN TABLE ft1 (i int) SERVER local_server OPTIONS (table_name 't1', m
 	g6 && psql -c "${sql}"
 }
 
+sql-remote-postgres-fdw-env(){
+	count=${1:-1000}
+	remote_sql="
+drop table IF EXISTS t1;
+
+create table t1(i int);
+insert into t1 select * from generate_series(1, ${count});
+
+	"
+
+	g6 && psql -h mdw-b -Ugpadmin -d gpadmin -c "${remote_sql}"
+
+	local_sql="
+drop foreign table IF EXISTS ft1;
+drop user mapping IF EXISTS for baotingfang SERVER mdw_b_server;
+drop server IF EXISTS mdw_b_server;
+drop extension IF EXISTS postgres_fdw;
+
+CREATE EXTENSION postgres_fdw;
+
+CREATE SERVER mdw_b_server FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'mdw-b', port '5432', dbname 'gpadmin');
+
+CREATE USER MAPPING FOR baotingfang
+	SERVER mdw_b_server
+	OPTIONS (user 'gpadmin', password '');
+
+CREATE FOREIGN TABLE ft1 (i int) SERVER mdw_b_server OPTIONS (table_name 't1', mpp_execute 'all segments');
+	"
+	g6 && psql -c "${local_sql}"
+}
+
