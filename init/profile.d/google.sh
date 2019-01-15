8ed1a791a420e5db27f02b4bf95c29032d1fbe65
Tingfang Bao <bbao@pivotal.io>
2019-01-15 13:22:16 +0800

    add gcp functions for login

--- init/profile.d/google.sh
+++ init/profile.d/google.sh
@@ -9,3 +9,14 @@ fi
 if [[ -f "${OPT}/Cells/google-cloud-sdk-218.0.0/completion.zsh.inc" ]]; then
 	source "${OPT}/Cells/google-cloud-sdk-218.0.0/completion.zsh.inc"
 fi
+
+gcp-login(){
+	gcloud auth login
+}
+
+gcp-login-workstation(){
+	gcloud compute \
+		--project "data-gp-toolsmiths" \
+		ssh --zone "asia-east1-a" "gpadmin@gp-toolsmiths-workstation-2" \
+		--ssh-flag=-A
+}
