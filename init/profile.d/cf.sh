b6553c65f70444dc0efc00bacce561b1c5a947c3
Tingfang Bao <mantingfangabc@163.com>
2019-03-20 12:44:59 +0800

    add cf-login-toolsmiths

--- init/profile.d/cf.sh
+++ init/profile.d/cf.sh
@@ -23,6 +23,15 @@ cf-login-books(){
 		--skip-ssl-validation
 }
 
+cf-login-toolsmiths(){
+	cf login -a  https://api.run.pivotal.io \
+		-u ${TOOLSMITHS_CF_USER} \
+		-p ${TOOLSMITHS_CF_PASS} \
+		-o "GP Toolsmiths" \
+		-s "development"
+}
+
+
 uaa-login-books(){
 	uaac target uaa.10.152.11.60.xip.io --skip-ssl-validation
 	uaac token client get ${UAA_CLIENT_ADMIN} -s ${UAA_CLIENT_PASS}
