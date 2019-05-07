848a68feab9369dd1e93f9a119afee721f83ff4d
Bob Bao <bbao@pivotal.io>
2019-05-07 17:10:20 +0800

    add pivotal docker

--- init/profile.d/mac/docker.sh
+++ init/profile.d/mac/docker.sh
@@ -1,76 +1,33 @@
 #!/usr/bin/env bash
 
-images="
-registry:2
+images=(
+alpine
+centos:6
+centos:7
+ubuntu:16.04
+ubuntu:18.04
+pivotaldata/gpdb6-centos6-build
+pivotaldata/gpdb6-centos6-test
+pivotaldata/gpdb6-centos7-build
+pivotaldata/gpdb6-centos7-test
+pivotaldata/gpdb6-centos6-dependencies-build
+pivotaldata/gpdb6-centos7-dependencies-build
 concourse/concourse
-ubuntu
-centos
-memcached
-redis
-mysql
-ruby
-nginx
-golang
-postgres
-python
-"
+)
 
-docker-container-clean(){
+docker-containers-clean(){
 	docker rm -fv $(docker ps -aq -f status=exited)
 }
 
-docker-disk-shrink(){
-	local cur_dir=`pwd`
-
-	cd "${OPT}/docker/"
-	/usr/local/bin/qemu-img convert -O qcow2 Docker.qcow2 Docker2.qcow2
-	mv Docker2.qcow2 Docker.qcow2
-	echo "done!"
-
-	cd ${cur_dir}
-}
-
-docker-disk-size(){
-	local cur_dir=`pwd`
-
-	cd "${OPT}/docker/"
-	du -sh ./Docker.qcow2
-
-	cd ${cur_dir}
-}
-
 docker-images-clean(){
-	docker rmi `docker images | grep "^<none>"|awk '{print $3}'` > /dev/null 2>&1
+	docker rmi `docker images | grep "<none>"|awk '{print $3}'`
 }
 
-docker-images-sync(){
+docker-images-pull(){
 	for image in ${images}
 	do
-		echo ""
 		echo "pulling $image ............"
 		docker pull "$image"
-
-		# echo ""
-		# echo "push $image to docker-registry............"
-		# tag="docker-registry.home:5000/$image"
-
-		echo ""
-		echo "TAG: $tag"
-		# docker tag "$image" "$tag"
-		# docker push "$tag"
-		# docker rmi "$tag"
-	done
-}
-
-docker-images-restore(){
-	for image in ${images}
-	do
-		echo ""
-		echo "pulling $image ............"
-		name="docker-registry.home:5000/$image"
-		docker pull "$name"
-		docker tag "$name" "$image"
-		docker rmi "$name"
 	done
 }
 
@@ -90,32 +47,4 @@ docker-redis(){
 		-p 6379:6379 \
 		redis \
 		redis-server --appendonly yes
-}
-
-docker-registry(){
-	local cur_dir=`pwd`
-	# docker run -d -p 5000:5000 --name docker-registry -v /Users/baotingfang/opt/docker/var/:/var/lib/registry registry:2
-	# echo "docker registry started"
-
-	# echo "pulling registry:2 image...."
-	# docker pull registry:2
-	# echo "Got Registry:2 Image [done]"
-
-	cd "${OPT}/docker/instances/docker-registry/"
-	docker-compose  $*
-	echo "docker-registry done!"
-
-	cd ${cur_dir}
-}
-
-docker-search(){
-	curl https://docker-registry.home:5000/v2/_catalog -k | jq
-
-	if [[ -n "$1" ]];then
-		curl https://docker-registry.home:5000/v2/$1/tags/list -k | jq
-	fi
-}
-
-docker-vm-enter(){
-	screen ${HOME}/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty
 }
