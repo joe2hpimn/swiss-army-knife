db8dbcc4f7f5987293aa85a03ceae9f0d00825d1
Bob Bao <bbao@pivotal.io>
2019-05-13 17:37:49 +0800

    update plumber

--- my-tools/lib/utils.py
+++ my-tools/lib/utils.py
@@ -4,7 +4,7 @@
 import sys
 import os
 
-import optparse
+import argparse
 
 
 def green(s):
@@ -23,12 +23,30 @@ def indent(count, str, indent_char='    '):
     return "".join([indent_char * count, str])
 
 
-def parse_options(description, parser_func, usage="usage: %prog [options]"):
+class StoreDict(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        kv = {}
+        if not isinstance(values, (list,)):
+            values = (values,)
+        for value in values:
+            n, v = value.split('=')
+            kv[n] = v
+        setattr(namespace, self.dest, kv)
+
+
+class MultiValues(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values.split(" "))
+
+
+def parse_options(description, parser_func):
     description = description
 
-    parser = optparse.OptionParser(
-        description=description, prog=os.path.basename(sys.argv[0]), usage=usage)
+    parser = argparse.ArgumentParser(
+        description=description, prog=os.path.basename(sys.argv[0]))
 
     parser_func(parser)
 
-    return parser.parse_args(sys.argv), parser
+    options = parser.parse_args()
+
+    return options, parser
